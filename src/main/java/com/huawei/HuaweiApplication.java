5cbb57180e20646093ea6ad3272085b62b725b19
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2025-01-09 22:22:57 +0300

    added mapperUtil for convert

--- src/main/java/com/huawei/HuaweiApplication.java
+++ src/main/java/com/huawei/HuaweiApplication.java
@@ -1,7 +1,9 @@
 package com.huawei;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HuaweiApplication {
@@ -10,4 +12,9 @@ public class HuaweiApplication {
         SpringApplication.run(HuaweiApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper mapper(){
+
+        return new ModelMapper();
+    }
 }
